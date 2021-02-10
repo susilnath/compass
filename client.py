08375a3ca8ceca2e723559ec9cc9394a5c54e459
crypto <crypto@hades.localdomain>
2021-02-10 22:44:24 +0530

    template created

--- client.py
+++ client.py
@@ -1,22 +1,54 @@
 #connection
 import socket
 
-import sys
+import sys,select
 import time
 
 port=8080
 
+class bcolors:
+    blue='\033[94m'
+    cyan='\033[96m'
+    green='\033[92m'
+    warning='\033[93m'
+    fail='\033[91m'
+    endc='\033[0m'
 soc=socket.socket()
 name=input("Enter username:")
 host=input("Enter the hostname:")
 soc.connect((host,port))
 print("[+] Connected to COMPASS")
+soc.send(name.encode())
 
+sys.stdout.write("\n$ ");sys.stdout.flush()
 while 1:
-    inp=soc.recv(1024)
-    inp=inp.decode()
-    print("[>] Server:",inp)
-    message=input(">>")
-    message=message.encode()
-    soc.send(message)
-    print("[+] Message sent!")
+    # inp=soc.recv(1024)
+    # inp=inp.decode()
+    # print("[>] Server:",inp)
+
+    # message=input(">>")
+    # message=message.encode()
+    # soc.send(message)
+    # print("[+] Message sent!")
+    # rec=soc.recv(1024)
+    # if rec:
+    #     print(rec.decode())
+    #     print('\n')
+
+    readc,writec,exceptc=select.select([sys.stdin,soc],[],[])
+    for conn in readc:
+        if conn==soc:
+
+            data=conn.recv(1024).decode()
+            if data:
+                print("\r"+bcolors.cyan+data+bcolors.endc)
+                sys.stdout.write("\033[34m"+'[Me :] '+ "\033[0m");sys.stdout.flush()
+            else:
+                time.sleep(1)
+                continue
+                #sys.exit("[-] Disconnected")
+        else:
+            inp=input("\033[34m"+'[Me :] '+ "\033[0m")
+            soc.send(inp.encode())
+            #sys.stdout.write("\033[34m"+'\n[Me :] '+ "\033[0m");sys.stdout.flush()
+        
--- server.py
+++ server.py
@@ -1,18 +1,23 @@
 import sys
+import select
 
 #connection
 import socket
 
 import time
 
-port=8080
-soc_list=[]
 #initialize
-
+port=8080
+soc_list_input=[]
+soc_list_output=[]
+msg_queue={}
+alive={}
+data={}
 
 #binding
 def server():
     soc=socket.socket()
+    soc.setblocking(0)
     soc.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     host=socket.gethostname()
     print("[+] Server will start on host: "+host)
@@ -22,17 +27,67 @@ def server():
     print("[-] Waiting for connections...")
 
     soc.listen(10)
-    soc_list.append(soc)
+    soc_list_input.append(soc)
     #incoming connection
-    connection,address=soc.accept()
-    print("[*] Connection made from ",address)
 
-    while 1:
-        inp=input(">>")
-        inp=inp.encode()
-        connection.send(inp)
-        print("[+] Message sent!")
 
-        received=connection.recv(1024)
-        received=received.decode()
-        print("[+] Received: "+received)
+    while soc_list_input:
+        # inp=input(">>")
+        # inp=inp.encode()
+        # connection.send(inp)
+        # print("[+] Message sent!")
+
+        readc,writec,exceptc=select.select(soc_list_input,soc_list_output,soc_list_input)
+
+        for conn in readc:
+            if conn is soc:
+                connection,address=soc.accept()
+                print("[*] Connection made from ",address)
+                soc_list_input.append(connection)
+                connection.setblocking(0)
+                name=connection.recv(1024).decode()
+                if name not in alive:
+                    alive[name]=connection
+                    data[name]=''
+                    print("[+] "+name+" joined")
+            else:
+                conn_name=list(alive.keys())[list(alive.values()).index(conn)]
+                data_rec=conn.recv(1024).decode()
+                if data_rec:
+                    print(">>> "+conn_name+" : "+data_rec)
+                    data[conn_name]=str(data[conn_name])+str(data_rec)
+                    soc_list_output.append(conn)
+                else:
+                    soc_list_input.remove(conn) #close socket if no data is read
+                    print("[-] Closed connection!")
+
+        for conn in writec:
+            conn_name=list(alive.keys())[list(alive.values()).index(conn)]#find the name from connection
+
+            to_send=data[conn_name]
+            broadcast(conn,to_send,conn_name)
+            # if conn_name:
+            #     if data[conn_name]:
+            #         conn.send(str(data[conn_name]).encode())
+            #         data[conn_name]=''
+            #         soc_list_output.remove(conn)
+        continue
+
+
+        # received=connection.recv(1024)
+        # received=received.decode()
+        # print("[+] Received: "+received)
+
+def broadcast(conn,to_send,owner):
+    for name in alive:
+        if alive[name]==conn:
+            continue
+        else:
+            send_conn=alive[name]
+            send_conn.send(str("["+owner+" :] "+to_send).encode())
+    data[owner]=''
+    soc_list_output.remove(conn)
+
+
+
+server()
