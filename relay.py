67f7090433945fd4d0921dee2fd5b1f54af72140
crypto <cycloners@gmail.com>
2021-02-13 10:39:51 +0530

    one relay

--- relay.py
+++ relay.py
@@ -1,1 +1,49 @@
 #start of relay branch in git
+
+import select
+import os
+
+import socket
+
+port=8081
+members=[]
+
+def server():
+    sock=socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    host=socket.gethostname()
+
+    sock.bind((host,port))
+    print("[+] Server will start on host: "+host)
+
+    sock.listen(10)
+    members.append(sock)
+
+    while 1:
+        readc,writec,exceptc=select.select(members,[],[])
+        
+        for conn in readc:
+            if conn==sock:
+                connection,address=conn.accept()
+                members.append(connection)
+                connection.setblocking(0)
+                print("[+] Adding "+str(address)+" to relay node")
+            else:
+                data=conn.recv(1024)
+                print(data)
+                if data:
+                    broadcast(sock,conn,data) 
+                else:
+                    members.remove(conn)
+                    print("[+] Server Disconnected!")
+
+def broadcast(sock,conn,to_send):
+    for i in members:
+        if conn==i:
+            continue
+        if i==sock:
+            continue
+        i.send(to_send)
+
+server()
+
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import time
 
 #connection
 import socket
+import pickle #to serialise data
 
 #initialize
 port=8080
@@ -13,11 +14,14 @@ msg_queue={}
 alive={}
 data={}
 
+rel_soc=0
+
 #binding
 def server():
     soc=socket.socket()
     soc.setblocking(0)
     soc.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    soc.setsockopt(socket.SOL_SOCKET,socket.SO_RCVBUF,4096)
     host=socket.gethostname()
     print("[+] Server will start on host: "+host)
 
@@ -28,6 +32,7 @@ def server():
     soc.listen(10)
     soc_list_input.append(soc)
 
+    global rel_soc
     #incoming connection
     while soc_list_input:
 
@@ -50,11 +55,19 @@ def server():
                             continue
                         connection.send(str(i+", ").encode())
 
+            elif conn == rel_soc:
+                rel_data_rec=conn.recv(4096)
+                sync(conn,1,rel_data_rec)
+
             else:
                 conn_name=list(alive.keys())[list(alive.values()).index(conn)]
                 data_rec=conn.recv(4096).decode()
                 if data_rec:
                     print(">>> "+conn_name+" : "+data_rec)
+
+                    #commands
+                    rel_soc=con_relay_temp()
+
                     data[conn_name]=str(data[conn_name])+str(data_rec)
                     soc_list_output.append(conn)
                 else:
@@ -79,9 +92,61 @@ def broadcast(conn,to_send,owner):
         else:
             send_conn=alive[name]
             send_conn.send(str("["+owner+" :] "+to_send).encode())
+    sync(rel_soc,2)
     data[owner]=''
     soc_list_output.remove(conn)
 
+def con_relay():
+    relay_addr="hades"
+    relay_port=8081
+    sock=socket.socket()
+    sock.connect((relay_addr,relay_port))
 
+    print("[#] Connected to relay - "+relay_addr+" : "+str(relay_port))
+    soc_list_input.append(sock)
 
+    return sock
+
+def serialise(syn_alive,syn_data):
+    ser_alive=pickle.dumps(syn_alive)
+    ser_data=pickle.dumps(data)
+
+    alive_length=str(sys.getsizeof(ser_alive)).rjust(4,'0')
+    alive_length=alive_length.encode()
+
+    return alive_length+ser_alive+ser_data
+
+def unserialise(rec_data):
+    alive_len=int(rec_data[:4].decode())
+    ser_alive=rec_data[4:][:alive_len]
+    ser_data=rec_data[alive_len:]
+
+    unser_alive=pickle.loads(ser_alive)
+    unser_data=pickle.loads(ser_data)
+
+    return unser_alive,unser_data
+
+def sync(sock,mode,rel_data=0):
+    #sync alive and data dicts across servers
+    if mode==1:#receive
+        print("[#] Received from relay!")
+        print(rel_data)
+        #rel_alive,rel_data=unserialise(rel_data)
+        #print(rel_alive)
+        #print(rel_data)
+    
+    if mode==2:#send
+        ser_send=serialise(list(alive.keys()),data)
+        sock.send(ser_send)
+        print("[#] Sent to relay successfully!")
+
+def con_relay_temp():
+    global a
+    if a==1:
+        sock=con_relay()
+        a=0
+        return sock
+    else:
+        return rel_soc
+a=1
 server()
